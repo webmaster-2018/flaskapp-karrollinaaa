0306054f3046f3466314a39d0009f227c598d93e
karrollinaaa <karolinab503@gmail.com>
2019-03-13 20:39:32 +0100

    dodanie bazy danych

--- uczniowie/modele.py
+++ uczniowie/modele.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
 #  modele.py
-
 from peewee import *
 
-baza_plik = 'quiz.db'
+baza_plik = 'uczniowie.db'
 baza = SqliteDatabase(baza_plik)  # instancja bazy
 
 ### MODELE #
+
+
 class BazaModel(Model):
     class Meta:
         database = baza
 
 
-class Uczen(BazaModel):
-    pass
+class Plec(BazaModel):
+    nazwa_plci = CharField(null=False)
 
 
 class Klasa(BazaModel):
-    pass
+    nazwa_klasy = CharField(null=False)
+    rok_naboru = IntegerField(null=False)
+    rok_matury = IntegerField(null=False)
 
 
-if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+class Uczen(BazaModel):
+    imie_ucznia = CharField(null=False)
+    nazwisko_ucznia = CharField(null=False)
+    plec_ucznia = ForeignKeyField(Plec, related_name='plec_ucznia')
+    klasa_ucznia = ForeignKeyField(Klasa, related_name='klasa_ucznia')
