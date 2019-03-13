f37adc9fa6972696e73e29b499850b6670ad96d9
karrollinaaa <karolinab503@gmail.com>
2019-03-13 20:58:17 +0100

    dodanie widoków list

--- uczniowie/views.py
+++ uczniowie/views.py
@@ -2,13 +2,28 @@
 # quiz-orm/views.py
 
 from flask import Flask
-from flask import render_template, request, redirect, url_for, abort, flash
+from flask import render_template, request, flash, redirect, url_for, abort
 from modele import *
 from forms import *
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    """Strona główna"""
     return render_template('index.html')
+
+
+@app.route("/lista_uczniow")
+def lista_uczniow():
+    uczen = (Uczen
+             .select(Uczen.id, Uczen.imie_ucznia, Uczen.nazwisko_ucznia, Plec.nazwa_plci, Klasa.nazwa_klasy)
+             .join_from(Uczen, Plec) 
+             .join_from(Uczen, Klasa, JOIN.LEFT_OUTER))
+    return render_template('uczniowie_lista.html', uczen=uczen)
+
+
+@app.route("/lista_klas")
+def lista_klas():
+    klasa = Klasa.select()
+    return render_template('klasy_lista.html', klasa=klasa)
